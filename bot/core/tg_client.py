9bf0c62d39de5adc6342aba6f690f322426e9657
rjriajul <rj@rj1.dev>
2026-09-20 12:58:57 +0530

    fix: pass TG_PROXY only when non-empty to support latest wzgram

--- bot/core/tg_client.py
+++ bot/core/tg_client.py
@@ -41,7 +41,8 @@ class TgClient:
     def wztgClient(cls, *args, proxy=None, **kwargs):
         kwargs["api_id"] = Config.TELEGRAM_API
         kwargs["api_hash"] = Config.TELEGRAM_HASH
-        kwargs["proxy"] = Config.TG_PROXY if proxy is None else proxy
+        if Config.TG_PROXY:
+            kwargs["proxy"] = Config.TG_PROXY if proxy is None else proxy
         kwargs["parse_mode"] = enums.ParseMode.HTML
         kwargs["in_memory"] = True
         for param, value in {
